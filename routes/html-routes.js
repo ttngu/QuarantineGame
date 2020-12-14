057fb7e96939fc0806eba3f3a21bed0c42b3fdb5
JohnWestermeyer <johnwestermeyer@gmail.com>
2020-12-14 14:20:21 -0800

    added things, and stuff

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,4 +3,11 @@ var path = require("path");
 // Routes
 // =============================================================
 module.exports = function(app) {
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+      });
+
+    app.get("/game", function(req, res){
+        res.sendFile(path.join(___dirname, "./public/games.html"));
+    });
 }
