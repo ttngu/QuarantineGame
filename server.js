b686061ee18e59dc677c4b5e5c323c8013dc1a6c
JohnWestermeyer <johnwestermeyer@gmail.com>
2020-12-15 11:19:30 -0800

    updating server file

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
 var express = require("express");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
@@ -28,8 +24,37 @@ require("./routes/html-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+
+db.sequelize.sync({ force: false, alter: true }).then(function() {
+  http.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);    
+  });
+});
+
+let lobbyCode = "";
+io.on('connection', function(socket){
+  //create new lobby     
+  socket.on('newGameLobby', data => {
+    lobbyCode = data.gameId;
+    if(lobbyCode.length > 0){
+      socket.join(lobbyCode);  
+    }
+  });
+  //join existing lobby
+  socket.on('joinLobby', code => {
+    if(io.sockets.adapter.rooms.get(lobbyCode).size >= 1){
+      lobbyCode = code;
+      socket.join(code);
+    } else {
+      //empty lobby fail condition TBD
+    }
+  });
+  //placeholder draw card
+  socket.on('drawCard', (card) => {
+    io.to(lobbyCode).emit('drawCardReturn', card)
+  });
+  //placeholder update scoreboard
+  socket.on('updateUserList', (userArr) => {
+    io.to(lobbyCode).emit('updateUserList', userArr)
   });
 });
