0b5e01ce8ca9b56f56cdc68425c39f766e3c425b
Tito Rivera <titorivera0707@gmail.com>
2020-12-15 15:00:32 -0800

    Updating index files.

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,33 @@
+const userInput = $('#username');
+const imageInput = $('#avatar');
+const lobbyInput = $('#lobby');
+
+$('.create').click(() => {
+    let name = userInput.val().trim();
+    let userArr = [];
+    console.log(name);
+    if(name){
+        userArr = [{name: name, avatar: 0, score: 0}];
+        localStorage.setItem("newLobby", JSON.stringify(userArr));
+        window.location.href = "/game";
+    } else {
+        //fail condition for trying to create lobby
+    }
+})
+
+$('.join').click(() => {
+    let name = userInput.val().trim();
+    let sentData = [];
+    console.log(name);
+    if(name){
+        sentData = [{name: name, avatar: 0, score: 0},lobbyCode];
+        localStorage.setItem("joinLobby", JSON.stringify(userArr));
+        window.location.href = "/game";
+    } else {
+        //fail condition for trying to create lobby
+    }
+})
+
 $(document).ready(function() {
     const valueSpan = $('.valueSpan');
     const value = $('#inputGroupSelect04');
@@ -8,3 +38,5 @@ $(document).ready(function() {
         }
     }).change();
   });
+
+  
