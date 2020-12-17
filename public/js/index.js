278423cf1aed9c46dabe18d1d0ad8c9464211a09
JohnWestermeyer <johnwestermeyer@gmail.com>
2020-12-17 09:51:19 -0800

    adding username and lobby code checks B-)

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,9 @@ const lobbyInput = $('#lobby');
 
 $('.create').click(() => {
     let avatar = imageInput.val();
+    if(avatar === "Choose Your Avatar"){
+        avatar = "trash";
+    }
     let name = userInput.val().trim();
     let userArr = [];
     if(name){
@@ -11,29 +14,34 @@ $('.create').click(() => {
         localStorage.setItem("newLobby", JSON.stringify(userArr));
         window.location.href = "/game";
     } else {
-        //fail condition for trying to create lobby
+        alert("Username Required to Join a Lobby");  
     }
 })
 
 $('.join').click(() => {
     let name = userInput.val().trim();
     let avatar = imageInput.val();
+    if(avatar === "Choose Your Avatar"){
+        avatar = "trash";
+    }
     let sentData = [];
     let lobbyCode = lobbyInput.val().trim();
-    console.log(name);
-    if(name){
+    if(!lobbyCode){
+        alert("Lobby Code Required to Join a Lobby");       
+    }else{ 
+        if(name){
         sentData = [{name: name, avatar: avatar, score: 0},lobbyCode];
         localStorage.setItem("joinLobby", JSON.stringify(sentData));
         window.location.href = "/game";
     } else {
-        //fail condition for trying to create lobby
-    }
+        alert("Username Required to Join a Lobby");  
+    }}
+    
 })
 
 $(document).ready(function() {
     const valueSpan = $('.valueSpan');
-    const value = $('#inputGroupSelect04');
-    console.log($(this).val());
+    const value = imageInput;
     value.change(function() {
         if($(this).val() !== "Choose Your Avatar"){
             valueSpan.html(`<img src="/images/avatar-icons/${$(this).val()}.png">`);
